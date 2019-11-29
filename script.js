0065d5fbe9b807fc19f911b604050d0830b19eb2
Vanchiendtvt <56372756+Vanchiendtvt@users.noreply.github.com>
2019-11-29 17:43:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 
-
 function startJS(){
     var imgBG = document.createElement('img');
     imgBG.src = 'http://wallarthd.com/wp-content/uploads/2015/10/Cool-Abstract-1920x1080.jpg'
-    
     window.addEventListener('load',function(event){
         var wapperDiv = document.getElementById('wapper');
-        wapperDiv.style.display = 'block';
+        wapperDiv.style.visibility = 'visible';
         var loadingP = document.getElementById('loading');
         loadingP.style.display = 'none';
         var backgroundImage = document.getElementById('backgroundImg');
         backgroundImage.style.backgroundImage = "url(http://wallarthd.com/wp-content/uploads/2015/10/Cool-Abstract-1920x1080.jpg)";
         backgroundImage.style.opacity = 1;
     });
-     
 }
 window.addEventListener('DOMContentLoaded',function(event){
     console.log('tai xong html');
